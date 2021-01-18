310a6ff61502cb3675f9f3eb947117b1575dd037
Wajd Mariam <wajd.mariam@mths.ca>
2021-01-18 23:13:33 +0000

    Initial Commit

---
+++ Palindrome.java
@@ -0,0 +1,100 @@
+/*
+* Copyright (c) 2021
+*
+* The program Palindrome.java uses recursion to find how many iterations 
+* it will take to find the palindrome number.
+* 
+* @author  Wajd Mariam
+* @version 1.0
+* @since   2021/01/18
+*/
+
+// Importing Scanners
+import java.util.Scanner; 
+
+public class Palindrome {
+  
+  
+  /**
+   * The reverseFunction function uses recursion 
+   * to reverse the received strings and pass them back.
+   */ 
+  public static String reverseFunction(String inputNumber) {
+    // Declaring variables used.
+    String stringReversed = "";
+    // Reversing string:
+    if (inputNumber.length() == 0) {
+      stringReversed = inputNumber;
+    } else {
+      return reverseFunction(inputNumber.substring(1)) + inputNumber.charAt(0);
+    }
+    return stringReversed;
+  }
+  
+  /**
+   * The plaindromeChecker method checks if the entered number and its reverse 
+   * are equal.
+   */ 
+  public static void palindromeChecker(String reversedString1, String comparedNumber,
+                                       int counter, String originalNumber1) {
+    
+    if (reversedString1.equals(comparedNumber)) {
+      System.out.println(comparedNumber + " is a palindrome!");
+      System.out.println("This means that " + originalNumber1 
+                          + " is depth " + counter + " palindrome!");
+      System.out.println("It took " + counter + " iterations to find its palindrome");
+    } else {
+      // Converting strings to integers.
+      int reversedNumber = Integer.parseInt(reversedString1);
+      int originalNumber = Integer.parseInt(comparedNumber);
+    
+      // Adding the two numbers to check if the sum is a palindrome or not.
+      int sum = reversedNumber + originalNumber;
+        
+      // Converting sum to string
+      String sumString = Integer.toString(sum);
+      
+      // Calling "reverseFunction" method to reverse the current sum.
+      String reversedString2 = reverseFunction(sumString);
+      
+      // Increasing counter every time a number is checked whether it is a palindrome.
+      counter = counter + 1;
+      
+      // Recall the same function "palindromeChecker"the check if the current 
+      // sum and it's reverse are equal (forms a palindrome).
+      palindromeChecker(reversedString2, sumString, counter++, originalNumber1);
+    }
+  }
+    
+  
+  /**
+   * The main function get input from user 
+   * and passes parameters to the methods.
+   */ 
+  public static void main(String[] args) {
+    // Declaring variables:
+    String stringInput = "";
+    int counter = 0;
+    
+    // Try Catch Statement.
+    try {
+      
+      // Getting input from users.
+      final Scanner input = new Scanner(System.in);
+      System.out.print("Enter a number to determine its palindrome: ");
+      stringInput = input.nextLine();
+      System.out.println("");
+      
+      // Calling "reverseFunction" method.
+      String reversedString = reverseFunction(stringInput);
+      
+      // Calling "palindromeChecker" method.
+      palindromeChecker(reversedString, stringInput, counter, stringInput);
+
+      // Catches errors.
+    } catch (Exception e) {
+      System.out.print("");
+      System.out.println("ERROR.");
+    }
+  }
+}
---
+++ Stars.java
@@ -0,0 +1,94 @@
+/*
+* Copyright (c) 2021
+*
+* The program Stars.java uses recursion to create a hourglass of stars 
+* of a size entered by the user.
+*
+* @author  Wajd Mariam
+* @version 1.0
+* @since   2021/01/18
+*/
+
+// Importing Scanners
+import java.util.Scanner; 
+
+public class Stars {
+  
+  /**
+   * This function prints the stars.
+   */ 
+  public static void printStars(int numberStars) {
+    // Printing stars
+    for (int printCounter = 0; printCounter < numberStars; printCounter++) {
+      System.out.print("*");
+    }
+    System.out.println("");
+  }
+  
+  /**
+   * The "starsDecreasing" method uses recursion to add stars.
+   */ 
+  public static void starsDecreasing(int number, int originalInput) {
+    if (number == 1) {
+      printStars(number);
+      starsIncreasing(number, originalInput);  
+    } else {
+      // Calling "printStars" method:
+      printStars(number);
+      // Recalling "starsDecreasing" method 
+      // again to add less stars than the previous time by 1.
+      starsDecreasing(number - 1, originalInput);
+    }
+  }
+      
+  /**
+   * The "starsIncreasing" method uses recursion to add stars.
+   */     
+  public static void starsIncreasing(int number1, int originalInput1) {
+    if (number1 == originalInput1) {
+      printStars(number1);
+      System.out.println("");
+      System.out.println("Done");
+    } else {
+      // Calling "printStars" function :
+      printStars(number1);
+      // Recalling "starsIncreasing" method 
+      // again to add more stars than the previous time by 1.
+      starsIncreasing(number1 + 1, originalInput1);   
+    }
+  }
+    
+  /**
+   * The main function gets input from user 
+   * and passes parameters to methods.
+   */ 
+  public static void main(String[] args) {
+    // Declaring variables used:
+    int intInput = 0;
+    
+    // Try Catch Statement.
+    try {
+      
+      // Getting input from users.
+      final Scanner input = new Scanner(System.in);
+      System.out.print("Enter a number to form a glass hour of it: ");
+      intInput = input.nextInt();
+      System.out.println("");
+      
+      // Checking if input == 1;
+      if (intInput == 1) {
+        throw null;
+      } else {
+        // Calling "starsDecreasing" method first since the hourglass starts from
+        // top and gradually decrease in size.
+        starsDecreasing(intInput, intInput);
+      }
+    } catch (NullPointerException e) {
+      System.out.println("ERROR: You can't make an hour glass with integer 1!");
+      // Catches errors.
+    } catch (Exception e) {
+      System.out.print("");
+      System.out.println("ERROR.");
+    }
+  }
+}
